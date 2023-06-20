651af0e4d4b2128de369b1fa33524ff97189153f
Jon Knutson <2101068+jonknutson@users.noreply.github.com>
2023-06-20 10:54:36 -0500

    Update entrypoint to fix container output (#13)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -l
 
-echo "Fetching kubeconfig and setting the context"
+echo "Fetching kubeconfig from EKS cluster '$CLUSTER' in AWS region '$REGION' and setting the kubectl context."
 aws eks update-kubeconfig --region $REGION --name $CLUSTER
 CONTEXT=$(aws eks describe-cluster --region $REGION --name $CLUSTER | jq -r .cluster.arn)
 kubectl config use-context $CONTEXT
 
-echo "Applying the selected manifest files"
+echo "Running kubectl $1:"
 kubectl $1
